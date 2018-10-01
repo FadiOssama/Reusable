3d077b1097e5733461980004c01402a66625e37c
Fadi Ramzy <fadi.ossama@pharos-solutions.de>
2018-10-01 10:59:18 +0200

    Initial Commit

---
+++ Reusable/Reusable.h
@@ -0,0 +1,19 @@
+//
+//  Reusable.h
+//  Reusable
+//
+//  Created by Fadi Ossama on 10/1/18.
+//  Copyright © 2018 Fadi Ossama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reusable.
+FOUNDATION_EXPORT double ReusableVersionNumber;
+
+//! Project version string for Reusable.
+FOUNDATION_EXPORT const unsigned char ReusableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reusable/PublicHeader.h>
+
+
