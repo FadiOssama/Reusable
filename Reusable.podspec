8125a3fd768c9f734d101da35af3b911fa90f572
Unknown <fadi.ossama@pharos-solutions.de>
2018-10-03 15:06:54 +0200

    Dequeue also supplementary view in collection view

--- Reusable.podspec
+++ Reusable.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "Reusable"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "Reuse tableview cell or collection view cell without indicating the reuse identifier by just telling the compiler the type of the cell instead of inferring it"
 
   # This description is used to generate tags and improve search results.
--- Reusable/UICollectionViewExtension.swift
+++ Reusable/UICollectionViewExtension.swift
@@ -13,4 +13,8 @@ public extension UICollectionView {
     final func dequeueReusableCell<T: Reusable>(indexPath: IndexPath) -> T! {
         return self.dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as? T ?? nil
     }
+    
+    final func dequeueReusableSupplementaryView<T: Reusable>(ofKind kind: String, indexPath: IndexPath) -> T! {
+        return self.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T ?? nil
+    }
 }
